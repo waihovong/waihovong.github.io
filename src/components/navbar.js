f7bd22465862fce9eb41d75288bbc2f8c2cd0c83
Wai Ho Vong <waihovong@Wais-Air.lan>
2020-08-22 23:19:30 +0930

    files

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => (
+    <nav className="navigation-bar">
+        <h1>Hello World</h1>
+        <h1>Hello World</h1>
+        <h1>Hello World</h1>
+        <h1>Hello World</h1>
+        <h1>Hello World</h1>
+        <h1>Hello World</h1>
+        <h1>Hello World</h1>
+    </nav>
+)
+
+export default NavBar
