72d62b8e0b4681b445080bda46f3913d0eece530
Wai Ho Vong <waihovong@Wais-Air.lan>
2020-08-26 01:55:30 +0930

    reponsive for mobiles

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,14 +7,14 @@ import "../styles/global.css"
 
 const NavBar = () => (
     <nav className="navigation-bar">
-        <Headroom>
+        {/* <Headroom> */}
             <ul className="navigation--header">
                 <AnchorLink to="/#home" className="navigation--option">Home</AnchorLink>
                 <AnchorLink to="/#projects" className="navigation--option">Projects</AnchorLink>
                 <li className="navigation--option">Contact</li>
                 <li className="navigation--option">Resume</li>
             </ul>
-        </Headroom>
+        {/* </Headroom> */}
     </nav>
 )
 
