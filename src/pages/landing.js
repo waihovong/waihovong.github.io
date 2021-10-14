f164c733485c422da527e7fabfaf48d415235894
Wai <waihovong@gmail.com>
2021-10-15 00:26:33 +1030

    Text Change

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -21,7 +21,7 @@ export default function LandingPage() {
                 Wai Ho Vong
             </div>
             <div className="about-section">
-                <p className="about--details">Hi, I'm Wai Ho! graduate Computer Science student at The University of Adelaide. <br/>
+                <p className="about--details">Hi, I'm Wai Ho! Graduate Computer Science student at The University of Adelaide. <br/>
                     Currently working full-time  as a graduate developer.<br /> 
                     I'm passionate about front-end development and have an interest in fullstack development. <br /> <br />
                     My hobbies include astronomy, listening to music, photography and Formula 1.
